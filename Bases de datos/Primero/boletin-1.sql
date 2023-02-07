1c0aab7f93874a665163443b3bd7db2fe0ddad11
Diego Rodríguez <121586270+drodriguez98@users.noreply.github.com>
2023-02-07 11:36:40 +0100

    Rename boletin1.sql to boletin-1.sql

--- Bases de datos/Primero/boletin-1.sql
+++ Bases de datos/Primero/boletin-1.sql
@@ -1,17 +1,17 @@
---  1. Create a PL/SQL script to print out the salary of employee called JONES from scott’s employee table. 
+--  1. Create a PL/SQL script to print out the salary of employee called JONES from scottâ€™s employee table. 
     -- Run the script under iSQL*PLUS. Make sure that you set up the SQL*PLUS output using SET SERVEROUTPUT ON.
 
 
 --  2. Modify the above script to print out a line of asterisks, then the salary information and then another line of asterisks.
 
---  3. Modify the script in Ex. 2 to parameterise the script so that any name could be entered to reveal that employee’s salary.
+--  3. Modify the script in Ex. 2 to parameterise the script so that any name could be entered to reveal that employeeâ€™s salary.
 
 --  4. Create a table called teacher with 3 or 4 columns. Create PL/SQL statement for inserting data into this table. 
     -- Obviously, the column values need to be parameterized.
 
 --  5. Modify the above to allow deletions of a row based on a condition.
 
---  6. Now create PL/SQL statements to double the salary of an employee based on the employee’s id given by the user.
+--  6. Now create PL/SQL statements to double the salary of an employee based on the employeeâ€™s id given by the user.
 
 
 --  7. (a) Create a PL/SQL script to print out the salary of all employees from the scott employee table using OPEN and FETCH 
