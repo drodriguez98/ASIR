123fc77a9b99bbad2681813cfb90b860edb894a5
Diego Rodríguez <121586270+drodriguez98@users.noreply.github.com>
2023-02-07 12:05:32 +0100

    Update bol-colexio.sql

--- Bases de datos/Segundo/bol-colexio.sql
+++ Bases de datos/Segundo/bol-colexio.sql
@@ -1,23 +1,36 @@
 --	Función que calcula a media de cada alumno e a amosa en texto.
 
-delimiter //
-create or replace function nota_media (nota1 decimal(10,2), nota2 decimal(10,2), nota3 decimal(10,2)) returns varchar(100)
-deterministic
-begin
-	declare media decimal(10,2);
-	set media = (nota1 + nota2 + nota3) / 3;
-	if (media < 3) then
-		return "Moi deficiente";
-	elseif (media >= 3) and (media < 5) then
-		return "Insuficiente";
-	elseif (media >= 5) and (media < 7) then
-		return "Suficiente";
-	elseif (media >= 7) and (media < 9) then
-		return "Notable";
-	elseif (media >= 9) then
-		return "Sobresaliente";
-	else
-		return "Nota no válida";
-	end if;
-end //
-delimiter;
+DELIMITER //
+
+CREATE OR REPLACE FUNCTION nota_media (nota1 decimal(10,2), nota2 decimal(10,2), nota3 decimal(10,2)) RETURNS varchar(100)
+
+DETERMINISTIC
+
+BEGIN
+
+	DECLARE media decimal(10,2);
+	
+	SET media = (nota1 + nota2 + nota3) / 3;
+	
+	IF (media < 3) THEN
+		RETURN "Moi deficiente";
+		
+	ELSEIF (media >= 3) and (media < 5) THEN
+		RETURN "Insuficiente";
+		
+	ELSEIF (media >= 5) and (media < 7) THEN
+		RETURN "SuficienteTHEN
+		
+	ELSEIF (media >= 7) and (media < 9) THEN
+		RETURN "Notable";
+		
+	ELSEIF (media >= 9) THEN
+		RETURN "Sobresaliente";
+		
+	ELSE
+		RETURN "Nota no válida";
+		
+	END IF;
+END //
+
+DELIMITER;
