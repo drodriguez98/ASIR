631584c83dc84edc50e5fb524f534867615c1154
Diego Rodríguez <121586270+drodriguez98@users.noreply.github.com>
2023-02-07 12:00:45 +0100

    Update bol-almacen.sql

--- Bases de datos/Segundo/bol-almacen.sql
+++ Bases de datos/Segundo/bol-almacen.sql
@@ -8,38 +8,38 @@ INSERT INTO productos (nombre, coste, precio, 1) VALUES ('Producto A', 4, 8, 2),
 
 --	Función que devuelve el beneficio de cada producto.
 
-delimiter //
-create or replace function beneficio (compra float, venta float) returns float
-deterministic
-begin
-	declare resultado float;
-	set resultado = venta-compra;
-	return resultado;
-end //
-delimiter ;
+DELIMITER //
+CREATE OR REPLACE FUNCTION beneficio (compra float, venta float) RETURNS float
+DETERMINISTIC
+BEGIN
+	DECLARE resultado float;
+	SET resultado = venta-compra;
+	RETURN resultado;
+END //
+DELIMITER ;
 
 	-- Para llamarla: select id, nombre, coste, precio, beneficio(coste, precio) as beneficio from produtos;
 	
 	
 --	Función que calcula con distintos porcentajes el precio de un producto según el tipo de iva que recibe como parámetro.
 
-delimiter //
-create or replace function beneficio (venta float, tipo tinyint) returns float
-deterministic
-begin
-	declare resultado float;
-	declare valor int;
-	if (tipo = 1) then
-		set valor = 4;
-		set resultado = venta * (1+4/100);
-	elseif (tipo = 2) then
-		set valor = 10;
-	elseif (tipo = 2) then
-		set valor = 21;
-	end if;	
-	set resultado = venta + (venta * valor) / 100;
-	return resultado;
-end //
-delimiter ;
+DELIMITER //
+CREATE OR REPLACE FUNCTION beneficio (venta float, tipo tinyint) RETURNS float
+DETERMINISTIC
+BEGIN
+	DECLARE resultado float;
+	DECLARE valor int;
+	IF (tipo = 1) THEN
+		SET valor = 4;
+		SET resultado = venta * (1+4/100);
+	ELSEIF (tipo = 2) then
+		SET valor = 10;
+	ELSEIF (tipo = 2) then
+		SET valor = 21;
+	END IF;	
+	SET resultado = venta + (venta * valor) / 100;
+	RETURN resultado;
+END //
+DELIMITER ;
 
 	-- Para llamarla: select id, nombre, coste, precio, beneficio(precio,tipo_iva) as precio_iva from produtos;
